f2725c1ed2a245cba6d55f27d62fb509dcfb0eef
Jonathan McNeil <creativeflow3@gmail.com>
2024-06-09 14:05:24 -0700

    Adding restaurant page, form, and api call

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,23 @@ export type User = {
   city: string;
   country: string;
 };
+
+export type MenuItem = {
+  _id: string;
+  name: string;
+  price: string;
+};
+
+export type Restaurant = {
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliverPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+};
